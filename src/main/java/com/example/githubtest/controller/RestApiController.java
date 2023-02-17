0ee7803e8e19ecadaca0dadc26c88d3d95454fa2
workspace <stylealist@gmail.com>
2023-02-17 14:58:57 +0900

    feature : getList method 추가
    getList method 추가

--- src/main/java/com/example/githubtest/controller/RestApiController.java
+++ src/main/java/com/example/githubtest/controller/RestApiController.java
@@ -1,7 +1,14 @@
 package com.example.githubtest.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestApiController {
+
+    @GetMapping(value = "/getList")
+    public String getList(){
+        return "getList Method";
+    }
+
 }
